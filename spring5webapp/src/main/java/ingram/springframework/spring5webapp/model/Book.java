9c32bca280818958851392b2dcf14cd39450959b
Adam Ingram <everett.ingram22@gmail.com>
2020-07-24 15:16:14 -0400

    Book POJO FIxed

--- spring5webapp/src/main/java/ingram/springframework/spring5webapp/model/Book.java
+++ spring5webapp/src/main/java/ingram/springframework/spring5webapp/model/Book.java
@@ -1,15 +1,19 @@
 package ingram.springframework.spring5webapp.model;
 
+import java.util.Set;
+
 public class Book {
     private String title;
     private String isbn;
+    private Set<Author> authors;
 
     public Book() {
     }
 
-    public Book(String title, String isbn) {
+    public Book(String title, String isbn, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
+        this.authors = authors;
     }
 
     public String getTitle() {
@@ -28,11 +32,20 @@ public class Book {
         this.isbn = isbn;
     }
 
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
                 "title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
                 '}';
     }
 }
