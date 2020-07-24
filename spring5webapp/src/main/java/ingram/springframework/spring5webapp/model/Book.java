4fd3d3c213895bf728e3c69b8eaadd773d274651
Adam Ingram <everett.ingram22@gmail.com>
2020-07-24 15:13:53 -0400

    Book Model POJO Created

--- spring5webapp/src/main/java/ingram/springframework/spring5webapp/model/Book.java
+++ spring5webapp/src/main/java/ingram/springframework/spring5webapp/model/Book.java
@@ -1,4 +1,38 @@
 package ingram.springframework.spring5webapp.model;
 
 public class Book {
+    private String title;
+    private String isbn;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
